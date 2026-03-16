3c909cd6675410a7c0f919d7c9ca69f3ef54223a
rhit-silkaijr <silkaijr@rose-hulman.edu>
2026-03-16 10:26:54 -0400

    Better input processing

--- points_based_on_input.m
+++ points_based_on_input.m
@@ -1,15 +1,20 @@
-function [P_o] = points_based_on_input(P,P2,n)
+function [P_o] = points_based_on_input(A,n)
 % points_based_on_input
-% Takes in a value P and P2, m x 2 vectors of points describing the top and
-% bottom of the wing respectively, with endpoints at 0 and 1
+% Takes in a value A, an m x 2 vector of points describing the wing,
+% with endpoints at 0, 1, and 0
+% Note A must have the descriptive line truncated in preprocessing
 % And n, the number of panels desired
 % Returns P_o, the points detailing each panel
+
 %n = 7;
 %P = [1 .95 .9 .8 .7 .6 .5 .4 .3 .25 .2 .15 .1 .075 .05 .025 .0125 0; .0019 .0155 .0281 .0508 .0702 .0865 .0989 .1071 .1088 .1065 .1015 .0934 .0805 .0717 .0603 .0445 .0328 0.000]';
 %P2 = [0 .0125 .025 .05 .075 .1 .15 .2 .25 .3 .4 .5 .6 .7 .8 .9 .95 1; 0 -.0245 -.0344 -.0468 -.0548 -.0603 -.0674 -.0709 -.0718 -.0712 -.0671 -.0599 -.0504 -.0397 -.0280 -.0153 -.0087 -.0019]';
 
-x = P(:,1);
-y = P(:,2);
+P1 = A(1:ceil(length(A)/2),:);
+P2 = A(ceil(length(A)/2):length(A),:);
+
+x = P1(:,1);
+y = P1(:,2);
 xx = 0:.01:1;
 x2 = P2(:,1);
 y2 = P2(:,2);
@@ -22,6 +27,8 @@ plot(x,y,'o',xx,yy);
 hold on
 
 plot(x2,y2,'g',xx,yy2);
+
+hold on
 
 P_o = zeros(n,2);
 
---
+++ testingInput.m
@@ -0,0 +1,2 @@
+A = importdata('naca2418.dat.txt');
+points_based_on_input(A,10)
